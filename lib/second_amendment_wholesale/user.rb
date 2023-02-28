97dde942ded2e996f8b3df9ffd0298a72f7c40c8
Kevin Brown <brownkm531@gmail.com>
2023-02-28 10:14:45 -0500

    just use user

--- lib/second_amendment_wholesale/user.rb
+++ lib/second_amendment_wholesale/user.rb
@@ -1,14 +1,26 @@
 module SecondAmendmentWholesale
   class User < Base
 
+    include SecondAmendmentWholesale::API
+
     def initialize(options = {})
       requires!(options, :token)
 
-      @client = SecondAmendmentWholesale::Client.new(token: options[:token])
+      @options = options
     end
 
     def authenticated?
-      @client.bearer_token.present?
+      headers = [
+        *auth_header(@options[:token]),
+        *content_type_header('application/json'),
+      ].to_h
+
+      response = get_request(
+        'token/validate',
+        headers
+      )
+
+      response.body.present?
     end
 
   end
