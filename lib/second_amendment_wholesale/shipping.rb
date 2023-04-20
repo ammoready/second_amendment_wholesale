3e1577f23c05a2460bf0f8ba49002a2dccdf8128
Kevin Brown <brownkm531@gmail.com>
2023-04-20 10:18:52 -0400

    just return the response

--- lib/second_amendment_wholesale/shipping.rb
+++ lib/second_amendment_wholesale/shipping.rb
@@ -17,23 +17,7 @@ module SecondAmendmentWholesale
       since_date = (since_date || Time.now.prev_day).strftime('%Y/%m/%d')
       endpoint = "shipmentsInformation/me?fromShipmentCreateAt=#{since_date}"
       
-      orders = get_request(endpoint, @headers).body
-
-      shipments = []
-
-      orders.each do |order|
-        order[:shipments].each do |shipment|
-          if shipment[:tracking_information].present?
-            tracking_data = { :order_id=>shipment[:order_id], :tracking_numbers=>[] }
-
-            shipment[:tracking_information].each_value do |tracking|
-              tracking_data[:tracking_numbers] << tracking[:track_number]
-            end
-
-            shipments << tracking_data
-          end
-        end
-      end
+      orders = get_request(endpoint, @headers)
     end
 
   end
